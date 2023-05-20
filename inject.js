5313f0e54da09928f40f5cad92c9e903ba351d47
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-05-20 13:44:42 -0400

    Injected XMLHttpRequest that intercepts network requests for liking / bookmarking tweets

---
+++ inject.js
@@ -0,0 +1,50 @@
+/*
+ *  This file injects a script to the DOM that handles the capturing and
+ *  intercepting of requests and responses.
+ *
+ */
+
+// Inject script.js into the DOM
+let script = document.createElement("script");
+script.src = chrome.runtime.getURL("script.js");
+script.onload = function () {
+  this.remove();
+};
+(document.head || document.documentElement).appendChild(script);
+
+// Listen for custom event dispatched from injected script.js
+document.addEventListener("storeTweetId", function (event) {
+  const type = event.detail.type;
+  const tweetId = event.detail.tweetId;
+  const tweetURL = `https://twitter.com/i/web/status/${tweetId}`
+
+  chrome.storage.local.get(["authToken"], function (items) {
+    const authToken = items.authToken;
+
+    if (authToken == null) {
+      console.error("Haven't set up Are.na account. Open extension and log in / authenticate.")
+    } else {
+      const LIKES_CHANNEL = "twitter-arena-likes-test";
+      const BOOKMARKS_CHANNEL = "twitter-arena-bookmarks-test";
+      const postToArenaURL = `https://api.are.na/v2/channels/${type == "favorite" ? LIKES_CHANNEL : BOOKMARKS_CHANNEL}/blocks?source=${tweetURL}`
+
+      try {
+        fetch(postToArenaURL, {
+          method: "POST",
+          headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${authToken}`
+          }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+      } catch (err) {
+        console.error("Error posting tweet to Are.na channel.")
+        console.error(err);
+      }
+    }
+  });
+});
